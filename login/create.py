dd2758f29dea7ee38c4a64696bef18eafebbc31a
Edward Tsang <edward.tsang99@gmail.com>
2015-06-11 03:12:05 -0400

    prepping for fixing 502 error

--- login/create.py
+++ login/create.py
@@ -53,14 +53,14 @@ def createAccount(form):
             password = md5Pass(password+user)
             f.write(user+","+password+"\n")
             f.close()
-            os.mkdir('../users/'+user)
-            os.chmod('../users/'+user, 0777)
-            os.mkdir('../users/'+user+'/sites')
-            os.chmod('../users/'+user+'/sites', 0777)
-            h = open('../users/'+user+'/sites/exist.txt',"w")
-            os.chmod('../users/'+user+'/sites/exist.txt', 0777)
-            h.write("site0")
-            h.close()
+            #os.mkdir('../users/'+user)
+            #os.chmod('../users/'+user, 0777)
+            #os.mkdir('../users/'+user+'/sites')
+            #os.chmod('../users/'+user+'/sites', 0777)
+            #h = open('../users/'+user+'/sites/exist.txt',"w")
+            #os.chmod('../users/'+user+'/sites/exist.txt', 0777)
+            #h.write("site0")
+            #h.close()
     else:
         result+="<h1>Invalid form submission, please fill in all fields</h1>"
     return result
