45a7d64fc13184e6753f45cf3d4df93805a5d3e5
Richard Lin <rlin5@stuy.edu>
2015-06-14 14:35:30 -0400

    Update usersites.py

--- usersites.py
+++ usersites.py
@@ -27,10 +27,10 @@ def listsites(x):
         stor = open("users/" + x + "/sites/exist.txt","r")
         y = stor.readlines()[1:]
         stor.close()
-        output += "<ol>"
+        output += "<ul class='uS'>"
         for i in y:
             output += "<li> <a href='users/" + x + "/sites/" + i + "'>"  + i + "</a> </li>"
-        return output + "</ol>"
+        return output + "</ul>"
 
 
 
@@ -61,7 +61,7 @@ final += '''</title>
 
 		<body>
 '''
-final +="		<h1>"
+final +='		<h1 class="USdirHeading">'
 final += key + "'s Sites </h1>"
 final += listsites(key)
 final += '''
