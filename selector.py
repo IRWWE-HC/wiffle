683a56f7d6675e9f52a3baeb54a0cd734d8b2019
Edward Tsang <edward.tsang99@gmail.com>
2015-06-14 01:05:01 -0400

    Fix selector.py navbar, NOTE: Fix navbar for logout, needs to be kind of a form?

--- selector.py
+++ selector.py
@@ -92,8 +92,11 @@ final += '''
 		  <nav class="navBar">
 			<a href="login/homepage.html" id="home"> Website Editor </a>
 			<ul class="navBarRight">
-				<li> <a href="http://bart.stuy.edu/~richard.lin"> Richard </a> </li>
-				<li> <a href="http://bart.stuy.edu/~edward.tsang"> Edward </a> </li>
+				<li> <a href="list.py"> Sites Directory
+				</a> </li>
+				<li> <a href="about.html"> About Us </a> </li>
+				<li> <a href="login/logout.py"> Sign Out
+				</a> </li>
 			</ul>
 		</nav>
         <form id="form" action="output.py" method="POST">
